f260fe0e0e5ab637178f1de424b3be2d46821fc2
Jairo Haro <xair_1319@hotmail.com>
2025-08-22 11:21:00 -0500

    limpieza de codigo

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,25 +1,3 @@
-/*const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
-dotenv.config();
-
-const createToken = (usuario) => {
-  if (!process.env.JWT_SECRET) {
-    throw new Error("JWT_SECRET no está definido");
-  }
-
-  const data = {
-    usuario_id: usuario.id,
-    usuario_usuario: usuario.usuario,
-    usuario_rol: usuario.rol,
-    usuario_nombre: usuario.nombre,
-  };
-
-  return jwt.sign(data, process.env.JWT_SECRET, { expiresIn: "1h" });
-};
-
-module.exports = { createToken };
-*/
-
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 dotenv.config();
