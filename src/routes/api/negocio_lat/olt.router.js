4c09ac3ab87601fcc21fdfc3ef539570d5e78486
Jairo <xair_1319@hotmail.com>
2026-02-12 20:00:11 -0500

    Prueba

--- src/routes/api/negocio_lat/olt.router.js
+++ src/routes/api/negocio_lat/olt.router.js
@@ -5,9 +5,9 @@ const { testTime } = require("../../../controllers/negocio_lat/olt.controller");
 // si usas checkToken:
 const { checkToken } = require("../../../utils/middlewares");
 
-router.get("/time", checkToken, testTime);
+//router.get("/time", checkToken, testTime);
 
 // si por ahora no quieres auth, temporalmente:
-// router.get("/time", testTime);
+router.get("/time", testTime);
 
 module.exports = router;
